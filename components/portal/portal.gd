c2e20d0822b7ef8b24eabfc1c2eac4db47b59975
Adamluczek <adamluk008@gmail.com>
2025-11-21 21:13:56 +0100

    working teleport

--- components/portal/portal.gd
+++ components/portal/portal.gd
@@ -1,12 +1,13 @@
 extends Area3D
-@onready var cube: CharacterBody3D = get_node("/root/World/Cube")
+
+@onready var cube: Node3D = get_parent().get_node("Cube")
 @export var teleport_destination : Area3D
 func _ready() -> void:
 	
 	cube.teleport.connect(on_teleport)
 		
 func on_teleport(colliderName):
-	
+	print("cos")
 	if self.name ==colliderName:
 		var destination = self.teleport_destination.global_position
 		cube.global_position = destination
