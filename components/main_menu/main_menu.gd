4061ed035cae005a755dfecd724c956e8f5dd572
Adamluczek <adamluk008@gmail.com>
2025-11-22 05:10:30 +0100

    add settings panel

--- components/main_menu/main_menu.gd
+++ components/main_menu/main_menu.gd
@@ -3,9 +3,12 @@ extends Control
 func _ready():
 	$VBoxContainer/PlayButton.pressed.connect(_on_play_pressed)
 	$VBoxContainer/QuitButton.pressed.connect(_on_quit_pressed)
-
+	$VBoxContainer/SettingsButton.pressed.connect(_on_setting_pressed)
 func _on_play_pressed():
 	GameManager.change_scene_with_fade("res://components/main_menu/tutorial.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
+func _on_setting_pressed():
+	GameManager.change_scene_with_fade("res://components/main_menu/settings.tscn")
+	
